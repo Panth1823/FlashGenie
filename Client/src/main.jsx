a3672a1153ae45903472b2c63e045f315560eda8
Panth1823 <panthshah242@gmail.com>
2024-08-18 18:26:16 +0530

    build(deps): update package-lock.json, package.json and main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
